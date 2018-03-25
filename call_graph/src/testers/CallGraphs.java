ea75647c01dc15fe8895f1095981823afeff93ad
Andrew Santosa <santosa_1999@yahoo.com>
2018-03-25 11:39:26 +0800

    Added a call graph over-approximation example in call_graph/src/testers/CallGraphs.java

--- call_graph/src/testers/CallGraphs.java
+++ call_graph/src/testers/CallGraphs.java
@@ -2,21 +2,39 @@ package testers;
 
 public class CallGraphs
 {
-	public static void main(String[] args) {
-		doStuff();
-	}
-	
-	public static void doStuff() {
-		new A().foo();
-	}
+    static private A field;
+    
+    public static void main(String[] args) {
+	field = new B();
+	doStuff();
+    }
+    
+    /**
+     * Here soot will declare that B.foo() can be called by doStuff(),
+     * where in reality it is not called by doStuff(), showing some
+     * imprecision.
+     */
+    public static void doStuff() {
+	new A().foo();
+    }
 }
 
 class A
 {
-	public void foo() {
-		bar();
-	}
+    public void foo() {
+	System.out.println("A.foo() executed.");
+	bar();
+    }
 	
-	public void bar() {
-	}
+    public void bar() {
+	System.out.println("A.bar() executed.");
+    }
+}
+
+class B extends A {
+    @Override
+    public void foo() {
+	System.out.println("B.foo() executed.");
+	bar();
+    }
 }
